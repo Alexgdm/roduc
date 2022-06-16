3b80c0115c2bd1c9c95c5ad4dbbeefcb7617bda1
Alexgdm <alexgustavodiesel@gmail.com>
2022-06-15 23:28:15 -0400

    comentados

--- restapi/admin.py
+++ restapi/admin.py
@@ -1,5 +1,5 @@
-from django.contrib import admin
-from RoducWeb.models import *
-# Register your models here.
+# from django.contrib import admin
+# from RoducWeb.models import *
+# # Register your models here.
 
-admin.site.register(Usuario)
+# admin.site.register(Usuario)
